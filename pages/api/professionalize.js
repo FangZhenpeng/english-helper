7a32793d4ec6261bec38288db9dabc1056422ceb
Zhenpeng Fang <zhenpeng@indeed.com>
2023-04-10 14:09:57 +0900

    Add translation, simplification and professionalization functions.

---
+++ pages/api/professionalize.js
@@ -0,0 +1,57 @@
+import {ChatGPTAPI} from "chatgpt";
+import clipboard from "clipboardy";
+
+const apiKey = process.env.OPENAI_API_KEY;
+
+const chatgpt = new ChatGPTAPI({
+    apiKey: apiKey,
+    completionParams: {
+        temperature: 0.3,
+        model: "gpt-3.5-turbo",
+    },
+    systemMessage: "My role is a Software Engineering Manager. Rephrase my sentences to sound more professional.",
+    debug: true,
+});
+
+export default async function (req, res) {
+    if (!apiKey) {
+        res.status(500).json({
+            error: {
+                message: "OpenAI API key not configured, please follow instructions in README.md",
+            }
+        });
+        return;
+    }
+
+    const text = req.body.text || '';
+    if (text.trim().length === 0) {
+        res.status(400).json({
+            error: {
+                message: "Please enter a valid text",
+            }
+        });
+        return;
+    }
+
+    try {
+        const response = await chatgpt.sendMessage(text);
+
+        // This clipboard will only function when the application is running on a local machine.
+        // It's recommended to use write() if you plan on hosting the application in the cloud.
+        clipboard.writeSync(response.text);
+        res.status(200).json({result: response.text});
+    } catch (error) {
+        // Consider adjusting the error handling logic for your use case
+        if (error.response) {
+            console.error(error.response.status, error.response.data);
+            res.status(error.response.status).json(error.response.data);
+        } else {
+            console.error(`Error with OpenAI API request: ${error.message}`);
+            res.status(500).json({
+                error: {
+                    message: 'An error occurred during your request.',
+                }
+            });
+        }
+    }
+}
--- pages/index.js
+++ pages/index.js
@@ -4,12 +4,14 @@ import styles from "./index.module.css";
 
 export default function Home() {
     const [textInput, setTextInput] = useState("");
+    const [buttonValue, setButtonValue] = useState("");
     const [result, setResult] = useState();
 
     async function onSubmit(event) {
         event.preventDefault();
         try {
-            const response = await fetch("/api/chatgenerate", {
+            const apiPath = "/api/" + buttonValue;
+            const response = await fetch(apiPath, {
                 method: "POST",
                 headers: {
                     "Content-Type": "application/json",
@@ -31,25 +33,31 @@ export default function Home() {
         }
     }
 
+
     return (
         <div>
             <Head>
-                <title>English Polisher</title>
+                <title>English Assistant</title>
                 <link rel="icon" href="/dog.png"/>
             </Head>
 
             <main className={styles.main}>
                 <img src="/dog.png" className={styles.icon}/>
-                <h3>Polish your English</h3>
+                <h3>Assist you in improving your English</h3>
                 <form onSubmit={onSubmit}>
                     <textarea
                         type="text"
                         name="textInput"
-                        placeholder="Enter an English sentence"
+                        placeholder="Enter a sentence"
                         value={textInput}
                         onChange={(e) => setTextInput(e.target.value)}
                     />
-                    <input type="submit" value="Polish"/>
+                    <div>
+                        <input type="submit" value="Polish" onClick={(e) => setButtonValue("polish")}/>
+                        <input type="submit" value="Translate" onClick={(e) => setButtonValue("translate")}/>
+                        <input type="submit" value="Simplify" onClick={(e) => setButtonValue("simplify")}/>
+                        <input type="submit" value="Professionalize" onClick={(e) => setButtonValue("professionalize")}/>
+                    </div>
                 </form>
                 <div className={styles.result}>{result}</div>
             </main>
